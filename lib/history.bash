90d82919b69b796234d9817e5b677d1ecf77ce88
Tamas Pal <folti78@gmail.com>
2014-07-16 10:10:41 +0200

    history: no history clutter when running under Midnight Commander

--- lib/history.bash
+++ lib/history.bash
@@ -7,6 +7,13 @@ export HISTCONTROL=${HISTCONTROL:-ignorespace:erasedups} # erase duplicates; alt
 export HISTSIZE=${HISTSIZE:-5000}                        # resize history size
 export AUTOFEATURE=${AUTOFEATURE:-true autotest}         # Cucumber / Autotest integration
 
+# ugly hack for Midnight Commander. It's needed to prevent MC to pollute the
+# history with 'cd "printf ...' lines for every directory change ..
+# See: http://www.midnight-commander.org/wiki/doc/faq#a6.8Iseelotofstrangecdprintflinesintomy.historyfile
+if [ -n "$MC_SID" ]; then
+  export HISTCONTROL=ignoreboth
+fi
+
 function rh {
   history | awk '{a[$2]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head
 }
